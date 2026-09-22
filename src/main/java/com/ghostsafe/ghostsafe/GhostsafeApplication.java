d91745b48d347090832b021aca61d4411a71bfe7
pedrohfilho <pedrohfilho251@gmail.com>
2026-09-21 21:16:36 -0300

    projeto Sprint boo com conexao ao banco

---
+++ src/main/java/com/ghostsafe/ghostsafe/GhostsafeApplication.java
@@ -0,0 +1,13 @@
+package com.ghostsafe.ghostsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostsafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhostsafeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ghostsafe/ghostsafe/GhostsafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghostsafe.ghostsafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostsafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
