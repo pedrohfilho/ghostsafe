a1308ea87faaf21cdda7f574dadaa3868b38fe6b
pedrohfilho <pedrohfilho251@gmail.com>
2026-09-23 13:07:06 -0300

    inico da interface do projeto

--- src/main/java/com/ghostsafe/ghostsafe/model/Projeto.java
+++ src/main/java/com/ghostsafe/ghostsafe/model/Projeto.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Table (name = "projetos")
 public class Projeto {
 
-    @id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
---
+++ src/main/java/com/ghostsafe/ghostsafe/repository/ProjetoRepository.java
@@ -0,0 +1,7 @@
+package com.ghostsafe.ghostsafe.repository;
+
+import com.ghostsafe.ghostsafe.model.Projeto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjetoRepository  extends JpaRepository<Projeto, Integer> {
+}
